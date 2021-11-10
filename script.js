484e30d1b848c6d85c24beb281c88a0fc5e7ee95
esatyasar <esatyasar91@gmail.com>
2021-11-11 00:55:51 +0300

    added modal-1 popup

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const openModal1 = document.querySelectorAll(".buttonModal-1")
-const modalContainer = document.querySelectorAll("#modalContainer1")
-const closeModal1 = document.querySelectorAll(".closeModal-1")
+const openModal1 = document.querySelector(".buttonModal-1")
+const modalContainer = document.querySelector("#modalContainer1")
+const closeModal1 = document.querySelector(".closeModal-1")
 
-openModal1.addEventListener('click', ()=> {
+openModal1.addEventListener("click", ()=> {
     modalContainer.classList.add("show")
 })
-close1.addEventListener('click', ()=> {
+closeModal1.addEventListener('click', ()=> {
     modalContainer.classList.remove("show")
 })
